83b7ffdd9fa525619b59c7cfa00728741c6247aa
Ali Badr AlJehani <abaljehani@tcc-ict.com>
2022-06-20 10:47:12 +0300

    adding authorization layer. and make sure everything is working perfectly

--- src/main/java/com/example/CRUDRESTapi/filter/CustomAuthorizationFilter.java
+++ src/main/java/com/example/CRUDRESTapi/filter/CustomAuthorizationFilter.java
@@ -31,17 +31,17 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 public class CustomAuthorizationFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        if (request.getServletPath().equals("/login")) {
+        if (request.getServletPath().equals("/api/login")) { //if this is the path requested, im not going to do anything and just let the user pass to the login page
             filterChain.doFilter(request, response);
         }else{
+            String authorizationHeader = request.getHeader(AUTHORIZATION); // authorization header is where the token stored
+            if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) { //the tokens usually starts with Bearer
                 try {
-                    String authorizationHeader = request.getHeader(AUTHORIZATION); // to get the path of the request
-                    if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
-                        String token = authorizationHeader.substring("Bearer".length()); //to get the token - the Bearer
+                        String token = authorizationHeader.substring("Bearer ".length()); //to get the token - the Bearer
                         Algorithm algorithm = Algorithm.HMAC256("secret".getBytes()); //has to be the same algorithm of the Authentication filter with the same key "secret"
-                        JWTVerifier verifier = JWT.require(algorithm).build();
-                        DecodedJWT decodedJWT = verifier.verify(token);
-                        String username = decodedJWT.getSubject();
+                        JWTVerifier verifier = JWT.require(algorithm).build(); //this is how we create the verifier
+                        DecodedJWT decodedJWT = verifier.verify(token); //pass the token to the verifier
+                        String username = decodedJWT.getSubject(); //this is going to give the username that comes in the token
                         String[] roles = decodedJWT.getClaim("roles").asArray(String.class); //.getClaim("what's the key word needs to be copied in the token "roles"") how to copy it .asArray(String.class)
                         Collection<SimpleGrantedAuthority> authorities = new ArrayList<>();
                         stream(roles).forEach(role -> {
@@ -50,18 +50,21 @@ public class CustomAuthorizationFilter extends OncePerRequestFilter {
                         UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(username, null, authorities);
                         SecurityContextHolder.getContext().setAuthentication(authenticationToken);
                         filterChain.doFilter(request, response);
-                    }
 
-                } catch (Exception exception) {
+                }catch (Exception exception) {
                     log.error("Error logging in: {}", exception.getMessage());
                     response.setHeader("error", exception.getMessage());
-                    //response.sendError(HttpServletResponse.SC_FORBIDDEN);
+                    response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+                    //  response.sendError(HttpServletResponse.SC_FORBIDDEN);
                     Map<String, String> error = new HashMap<>();
                     error.put("error_message", exception.getMessage());
                     response.setContentType(APPLICATION_JSON_VALUE);
                     new ObjectMapper().writeValue(response.getOutputStream(), error); // this going to return the tokens in a JSON format in the body of the response
 
                 }
+                }else {
+                filterChain.doFilter(request,response);
+            }
             }
         }
     }
--- src/main/java/com/example/CRUDRESTapi/security/SecurityConfig.java
+++ src/main/java/com/example/CRUDRESTapi/security/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.example.CRUDRESTapi.security;
 
 import com.example.CRUDRESTapi.filter.CustomAuthenticationFilter;
+import com.example.CRUDRESTapi.filter.CustomAuthorizationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,6 +12,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import static org.springframework.http.HttpMethod.*;
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
@@ -30,10 +32,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     }
     @Override
     protected void configure(HttpSecurity http) throws Exception { //further config can be implemented to authorize the login page check 1:25:00
+        CustomAuthenticationFilter customAuthenticationFilter = new CustomAuthenticationFilter(authenticationManagerBean());
+        customAuthenticationFilter.setFilterProcessesUrl("/api/login"); // now instead of going to ("localhost:8081/login") we will go to ("localhost:8081/api/login")
         http.csrf().disable();
         http.sessionManagement().sessionCreationPolicy(STATELESS); //|| SESSIONS //understand the difference 1:03:00
+        http.authorizeRequests().antMatchers("/api/login/**").permitAll();
 //        http.authorizeRequests().anyRequest().permitAll(); //allow anyone to access this application, this is not practical because we need to make some end-points accessed by a certain user_Roles ex: Super_Admin
-        http.authorizeRequests().antMatchers(GET,"api/users/**").hasAuthority("SUPER_ADMIN");
+        http.authorizeRequests().antMatchers(GET,"api/users/**").hasAuthority("SUPER_ADMIN"); // check the path in the controllers
         http.authorizeRequests().antMatchers(GET,"api/users/**").hasAuthority("ADMIN");
         http.authorizeRequests().antMatchers(GET,"api/employees").hasAuthority("MANAGER");
         http.authorizeRequests().antMatchers(GET,"api/employees/**").hasAuthority("MANAGER");
@@ -51,8 +56,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.authorizeRequests().antMatchers(DELETE,"api/users/**").hasAuthority("ADMIN");
         http.authorizeRequests().antMatchers(DELETE,"api/employees/**").hasAuthority("SUPER_ADMIN");
         http.authorizeRequests().antMatchers(DELETE,"api/employees/**").hasAuthority("ADMIN");
-        http.authorizeRequests().anyRequest().authenticated().antMatchers(GET,"api/products/**");
-        http.authorizeRequests().antMatchers(GET,"api/products/**");
+        http.authorizeRequests().antMatchers(GET,"api/products/**").hasAnyAuthority();
+        http.authorizeRequests().antMatchers(GET,"api/branches/**").hasAnyAuthority();
         http.authorizeRequests().antMatchers(POST,"api/products/**").hasAuthority("SUPER_ADMIN");
         http.authorizeRequests().antMatchers(POST,"api/products/**").hasAuthority("ADMIN");
         http.authorizeRequests().antMatchers(POST,"api/products/**").hasAuthority("MANAGER");
@@ -62,16 +67,21 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.authorizeRequests().antMatchers(DELETE,"api/products/**").hasAuthority("MANAGER");
         http.authorizeRequests().antMatchers(DELETE,"api/products/**").hasAuthority("EMPLOYEE");
 
-
-
-
-
-        http.authorizeRequests().anyRequest().authenticated().and().httpBasic();
-        http.addFilter(new CustomAuthenticationFilter(authenticationManagerBean()));
+        http.authorizeRequests().anyRequest().authenticated();
+        //http.authorizeRequests().anyRequest().authenticated().and().httpBasic();
+        http.addFilter(customAuthenticationFilter);
+        http.addFilterBefore(new CustomAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
     }
-    @Bean
+     @Bean
     @Override
     public AuthenticationManager authenticationManagerBean() throws Exception {
         return super.authenticationManagerBean();
     }
 }
+
+
+/*
+* the access_token is used to access resources by the client
+* whenever the access token is required the application on the front end will ask if a re-fresh token is available, so the user can continue accessing the resources*/
+
+//*implement re-fresh token 1:58:00 https://www.youtube.com/watch?v=mYKf4pufQWA&t=7003s
